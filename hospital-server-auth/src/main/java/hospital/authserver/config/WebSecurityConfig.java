35fb0f22c4de4cecb6e7f8de8ce01d22fee16566
夏柒柒xzy <2316847001@qq.com>
2020-04-22 09:11:33 +0800

    test

--- hospital-server-auth/src/main/java/hospital/authserver/config/WebSecurityConfig.java
+++ hospital-server-auth/src/main/java/hospital/authserver/config/WebSecurityConfig.java
@@ -1,22 +1,14 @@
 package hospital.authserver.config;
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-import org.springframework.security.web.csrf.CsrfFilter;
 import hospital.authserver.config.service.UserDetailsServiceImpl;
 
 @Configuration
@@ -35,30 +27,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
     @Override
     public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/oauth/check_token","/favicon.ico");
-    }
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/oauth2/authorization/github").permitAll()
-                .antMatchers("/oauth2/authorization/gitee").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/custom/login")
-                .loginProcessingUrl("/login")
-                .successHandler(new SavedRequestAwareAuthenticationSuccessHandler() {
-                    @Override
-                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                                                        Authentication authentication) throws ServletException, IOException {
-                        super.onAuthenticationSuccess(request, response, authentication);
-                    }
-                })
-                .permitAll()
-                .and()
-                .httpBasic()
-                .and()
-                .addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class);//把CSRFtoken设定到cookie;
+        web.ignoring().antMatchers("/oauth/check_token");
     }
 }
